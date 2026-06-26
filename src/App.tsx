957e2e0b1d83eb59bf4b954d6235e545a45ca8cb
Konstantin Mikheev <mik.kon6610@gmail.com>
2026-06-26 13:39:25 +0200

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ import hbImg from './assets/hb.png';
 import { listItems, phoneSlides } from './data';
 import { LS, LSKeys } from './ls';
 import { appSt } from './style.css';
+import { sendDataToGA } from './utils/events';
 
 const LINK =
   'alfabank://sdui_screen?endpoint=v1%2Fgrowthhack-widget-experiment%2Fwidgets%2F0c1eaaaa-f56e-4e81-8f3a-1043f0025e0f&presentationTypeWeb=PRESENT&title=%D0%A8%D0%B0%D1%85%D0%BC%D0%B0%D1%82%D1%8B&screenName=loyalty_1_final';
@@ -27,7 +28,12 @@ export const App = () => {
   const submit = () => {
     setLoading(true);
 
-    window.location.replace(LINK);
+    sendDataToGA({
+      event: 'click',
+      sub_set: '5,2',
+    }).then(() => {
+      window.location.replace(LINK);
+    });
   };
 
   return (
